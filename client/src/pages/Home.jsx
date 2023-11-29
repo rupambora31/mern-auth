918dc73707de7d3963008462c98c0c31b59fe766
rupambora31 <iamrupam99@gmail.com>
2023-11-29 09:32:44 +0530

    complete Home and About pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold  mb-4 text-slate-800">
+        Welcome to my first full-stack web app!
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Hello! I'm Rupam, hailing from the serene lands of Assam, India.
+        Stepping into the realm of web development, I've embarked on my
+        inaugural journey into the captivating world of full-stack development
+        using the MERN stack. As a newcomer in this dynamic field, every line of
+        code written, each milestone achieved, resonates with the thrill of
+        discovery and learning. With a blend of curiosity and dedication, I
+        traverse through the complexities of front-end and back-end development,
+        crafting my first MERN project. Embracing the challenges, I eagerly
+        embrace every opportunity to expand my skills, unravel the mysteries of
+        JavaScript, Node.js, React, and MongoDB, and bring forth innovative
+        solutions in this ever-evolving landscape of technology.
+      </p>
+    </div>
+  );
 }
